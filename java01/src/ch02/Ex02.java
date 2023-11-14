60daa74b48645b08b6bfbd30f0c92d0fa92a195d
jsm <jsm@DESKTOP-NHN3D6A>
2023-11-14 21:45:03 +0900

    comment delete2

--- java01/src/ch02/Ex02.java
+++ java01/src/ch02/Ex02.java
@@ -16,7 +16,6 @@ public class Ex02 {
 		list.add(new Member(24,"jein","woman","010-5555-7777")) ;
 		list.forEach(System.out::println);
 		list.forEach(m  -> System.out.print("Age : "+m.getAge()+", Name : "+m.getName()+", Gender : "+m.getGender()+", Tel : "+m.getTel()+"\n"));
-		//tiger comment add 1
-		//tiger comment add 2
+
 	}
 }
