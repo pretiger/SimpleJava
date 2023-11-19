e85bb0b43b125ecfdd65f6d37e206d852ee56687
jsm <jsm@DESKTOP-NHN3D6A>
2023-11-19 12:32:58 +0900

    main comment add 2

--- java01/src/ch02/Ex02.java
+++ java01/src/ch02/Ex02.java
@@ -16,5 +16,7 @@ public class Ex02 {
 		list.add(new Member(24,"jein","woman","010-5555-7777")) ;
 		list.forEach(System.out::println);
 		list.forEach(m  -> System.out.print("Age : "+m.getAge()+", Name : "+m.getName()+", Gender : "+m.getGender()+", Tel : "+m.getTel()+"\n"));
+		//main comment add 1
+		//main comment add 2
 	}
 }
